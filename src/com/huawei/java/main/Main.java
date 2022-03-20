5013fc2a7ed34e559ba36a0e72b9e57c3412a2ee
gh <1179681903@qq.com>
2022-03-20 13:41:33 +0800

    baseline

---
+++ src/com/huawei/java/main/Main.java
@@ -0,0 +1,9 @@
+package com.huawei.java.main;
+public class Main {
+
+	public static void main(String[] args) {
+		int[] nums = new int[]{57,57,57,57,57,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,85,85,85,86,86,86};
+		System.out.println(countHillValley(nums));
+	}
+
+}
